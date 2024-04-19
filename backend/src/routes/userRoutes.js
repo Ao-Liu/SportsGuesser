2b5d3617b1dbcb446f2378b4280336cbd8f1e851
hsinyac <hsinyac@andrew.cmu.edu>
2024-04-19 14:37:29 -0400

    create userInfo once after login using Oauth

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -19,37 +19,71 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.post('/create', async (req, res) => {
+    console.log("inininininin");
+    console.log(req.body.uid);
+
+    const user = await User.findOne({ uid: req.body.uid });
+    if (!user) {
+        console.log("User does not exist yet");
+        const newUser = new User({
+            uid: req.body.uid,
+            displayName: req.body.displayName,
+            email: req.body.email,
+            photoURL: req.body.photoURL,
+            numGamesCompleted: req.body.numGamesCompleted || 0,
+            numGamesWon: req.body.numGamesWon || 0,
+            // conqueredCourNameUrl: [
+            //     { name: 'Staples Center Lakers', url: 'https://c8.alamy.com/comp/AR6H8X/nba-la-lakers-staple-center-los-angeles-california-usa-AR6H8X.jpg' },
+            //     { name: 'Pittsburgh Penguins Hockey', url: 'https://www.discovertheburgh.com/wp-content/uploads/2018/04/20180411_200128-600px.jpg' },
+            // ],
+        });
+
+        try {
+            newUser = await user.save();
+            res.status(201).json(newUser);
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
+    }
+    console.log("User already exist.");
+    console.log("newUser: ", newUser);
+    console.log("user: ", user.schema.paths.uid);
+    res.status(201).json({});
+});
 
 /**
  * Retrieves a user by uid.
  */
 router.get('/:uid', async (req, res) => {
-    console.log("backend backend")
+    console.log("backend backend looking for Uid: ", req.params.uid)
     const { uid } = req.params;
-    
+
     ////////////// Temporary dummy data ////////////
-    const dummyUser = {
-        uid: 1,
-        displayName: "Scotty",
-        email: "scotty@gmail",
-        photoURL: "https://www.cmu.edu/brand/brand-guidelines/images/scottycrop2-600x600.png",
-        numGamesCompleted: 10,
-        numGamesWon: 2,
-        conqueredCourNameUrl: [
-            { name: 'Staples Center Lakers', url: 'https://c8.alamy.com/comp/AR6H8X/nba-la-lakers-staple-center-los-angeles-california-usa-AR6H8X.jpg' },
-            { name: 'Pittsburgh Penguins Hockey', url: 'https://www.discovertheburgh.com/wp-content/uploads/2018/04/20180411_200128-600px.jpg' },
-        ],
-    };
+    // const dummyUser = {
+    //     uid: 1,
+    //     displayName: "Scotty",
+    //     email: "scotty@gmail",
+    //     photoURL: "https://www.cmu.edu/brand/brand-guidelines/images/scottycrop2-600x600.png",
+    //     numGamesCompleted: 10,
+    //     numGamesWon: 2,
+    //     conqueredCourNameUrl: [
+    //         { name: 'Staples Center Lakers', url: 'https://c8.alamy.com/comp/AR6H8X/nba-la-lakers-staple-center-los-angeles-california-usa-AR6H8X.jpg' },
+    //         { name: 'Pittsburgh Penguins Hockey', url: 'https://www.discovertheburgh.com/wp-content/uploads/2018/04/20180411_200128-600px.jpg' },
+    //     ],
+    // };
     ////////////// Temporary dummy data ////////////
 
     try {
-        // const user = await User.findOne({ uid: uid });
-        // if (!user) {
-        //     return res.status(404).json({ message: "User not found." });
-        // }
-        
-        // res.json(user);
-        res.json(dummyUser);
+        console.log("looking for uid: ", uid)
+        const user = await User.findOne({ uid: uid });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found." });
+        }
+
+        res.json(user);
+        // res.json(dummyUser1);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
@@ -65,7 +99,7 @@ router.put('/update-completed/:uid', async (req, res) => {
     const { numGamesCompleted } = req.body;
     try {
         const user = await User.findOneAndUpdate(
-            { uid: uid }, 
+            { uid: uid },
             { $set: { numGamesCompleted: numGamesCompleted } },
             { new: true }
         );
@@ -86,7 +120,7 @@ router.put('/update-won/:uid', async (req, res) => {
     const { numGamesWon } = req.body;
     try {
         const user = await User.findOneAndUpdate(
-            { uid: uid }, 
+            { uid: uid },
             { $set: { numGamesWon: numGamesWon } },
             { new: true }
         );
